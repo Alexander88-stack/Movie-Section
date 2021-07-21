833afbf513347fac4595e032ddc4dcd09f4ec71b
Alexandru <>
2021-07-21 16:07:04 +0200

    using GET from same api

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-http-3f652-default-rtdb.europe-west1.firebasedatabase.app/movies.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
        };
@@ -23,17 +23,19 @@ function App() {
 
       const data = await response.json();
 
-     
+     const loadedMovies = [];
+
+     for (const key in data) {
+       loadedMovies.push({
+         id: key,
+         title: data[key].title,
+         openingText: data[key].openingText,
+         releaseDate: data[key].releaseDate
+       });
+     }
     
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        };
-      });
-     setMovies( transformedMovies );
+      
+     setMovies( loadedMovies );
      
 
     } catch (error){
